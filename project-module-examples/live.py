54f339fa53770cf5c8b80987243b1e32de2125ff
Ian Clester <ijc@ijc8.me>
2023-01-24 17:22:41 -0500

    Experiment with using Vortex in expressions

--- project-module-examples/live.py
+++ project-module-examples/live.py
@@ -29,3 +29,37 @@ def t(pattern, *args, **kwargs):
 
 def transpose(notes, offset):
     return [{**note, "pitch": note["pitch"] + offset} for note in notes]
+
+# Experiment with Vortex for Tidal notation.
+# (Requires latest: `pip install "tidalvortex @ git+https://github.com/tidalcycles/vortex@main"`)
+import re
+import numbers
+import vortex
+from vortex import note  # for use in expressions
+
+note_regex = re.compile("([A-Ga-g])([#b]?)(\d*)")
+note_names = "cdefgab"
+major_scale = [0, 2, 4, 5, 7, 9, 11]
+accidentals = {'': 0, '#': 1, 'b': -1}
+
+def note_to_pitch(note):
+    if isinstance(note, numbers.Real):
+        return note
+    name, accidental, octave = note_regex.match(note).groups()
+    name = name.lower()
+    accidental = accidentals[accidental]
+    octave = int(octave) if octave else 4
+    return major_scale[note_names.index(name)] + accidental + (octave + 1) * 12
+
+# TODO: Try custom converter for `vortex.Pattern` instead of using `n()`;
+#       consider using length of item to determine number of cycles rather than taking argument.
+# Ex:
+#   =n("c d e g")
+#   =n("c [e g] <a*3 b*3> c5*4", 2)
+#   =n(note("e d c d e e e ~").slow(2), 2)
+def n(pattern, cycles=1):
+    if isinstance(pattern, str):
+        pattern = vortex.note(pattern)
+    for event in pattern.query(vortex.TimeSpan(0, cycles)):
+        # TODO: Respect cycle length / project time signature
+        yield {"pitch": note_to_pitch(event.value["note"]), "start": float(event.part.begin), "end": float(event.part.end)}
