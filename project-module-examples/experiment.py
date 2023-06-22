e6b537642fd58e7db08f3de9e1530d206e988a8c
Ian Clester <ijc@ijc8.me>
2023-06-22 00:14:03 -0400

    REPL history: up/down work

--- project-module-examples/experiment.py
+++ project-module-examples/experiment.py
@@ -61,21 +61,77 @@ except NameError:
 scroll_to_bottom = False
 
 from code import InteractiveInterpreter
-interp = InteractiveInterpreter()
+interp = InteractiveInterpreter(globals())
 
 COLORS = {
     "input": 0xFFCC99FF,
-    "output": 0x6699FFFF,
+    "output": 0x6699FFFF, # 0xFFFFFFFF,
     "error": 0xFF6666FF,
 }
 
+# See console demo from https://github.com/cfillion/reaimgui/blob/v0.8.6.1/examples/demo.lua
+# TODO: consider some fancy stuff for auto-reloading/upgrading instances of classes
+class Console:
+    def __init__(self):
+        self.history = [""]
+        self.history_pos = 0
+        # Unfortunately, can't pass a Python callback into ReaImGui functions... so this bit is written in EEL.
+        self.callback = ImGui_CreateFunctionFromEEL("""
+EventFlag == InputTextFlags_CallbackHistory ? (
+    prev_history_pos = HistoryPos;
+    history_line = #;
+    EventKey == Key_UpArrow && HistoryPos > 0 ? (
+        HistoryPos -= 1;
+        strcpy(history_line, #HistoryPrev);
+    );
+    EventKey == Key_DownArrow && HistoryPos < HistorySize - 1 ? (
+        HistoryPos += 1;
+        strcpy(history_line, #HistoryNext);
+    );
+    prev_history_pos != HistoryPos ? (
+        InputTextCallback_DeleteChars(0, strlen(#Buf));
+        InputTextCallback_InsertChars(0, history_line);
+    );
+);
+""")
+        flags = [
+            'InputTextFlags_CallbackHistory',
+            'Key_UpArrow', 'Key_DownArrow',
+        ]
+        for flag in flags:
+            ImGui_Function_SetValue(self.callback, flag, globals()["ImGui_" + flag]())
+
+    def pre_callback(self):
+        ImGui_Function_SetValue(self.callback, 'HistoryPos', self.history_pos)
+        ImGui_Function_SetValue(self.callback, 'HistorySize', len(self.history))
+        ImGui_Function_SetValue_String(self.callback, '#HistoryPrev', self.history[self.history_pos == 0 and len(self.history) - 1 or self.history_pos - 1] if self.history else '')
+        ImGui_Function_SetValue_String(self.callback, '#HistoryNext', self.history[self.history_pos + 1] if self.history_pos + 1 < len(self.history) else '')
+
+    def post_callback(self):
+        self.history_pos = int(ImGui_Function_GetValue(self.callback, 'HistoryPos'))
+
+    def exec(self, command):
+        self.history[-1] = command
+        self.history.append("")
+        self.history_pos = len(self.history) - 1
+    
+    def draw(self, ctx, label):
+        self.pre_callback()
+        changed, contents = ImGui_InputText(ctx, label, input_string, ImGui_InputTextFlags_CallbackHistory(), self.callback)
+        self.post_callback()
+        if self.history_pos == len(self.history) - 1:
+            # Save edits to latest line (so user can visit history without losing in-progress command).
+            # If we wanted to make this more like readline, we'd do this with all the history while also saving the original commands (to be restored after executing).
+            self.history[self.history_pos] = contents
+        return changed, contents
+
 def loop():
-    global input_string, module_source, history, scroll_to_bottom
+    global input_string, module_source, scroll_to_bottom, console, history
     visible, open = ImGui_Begin(ctx, 'My window', True)
     if visible:
         try:
             ImGui_PushItemWidth(ctx, -1)
-            _, module_source = ImGui_InputTextMultiline(ctx, "##module_source", module_source, 0, 0) #, 0, 0, ImGui_InputTextFlags_AllowTabInput())
+            _, module_source = ImGui_InputTextMultiline(ctx, "##module_source", module_source, 0, 0, ImGui_InputTextFlags_AllowTabInput())
             ImGui_PopItemWidth(ctx)
 
             if ImGui_BeginChild(ctx, 'ScrollingRegion', 0, -20, False, ImGui_WindowFlags_HorizontalScrollbar()):
@@ -91,13 +147,14 @@ def loop():
             ImGui_Text(ctx, ">>>")
             ImGui_SameLine(ctx)
             ImGui_PushItemWidth(ctx, -1)
-            _, input_string = ImGui_InputText(ctx, "##code", input_string)
+            _, input_string = console.draw(ctx, "##code")
             ImGui_PopItemWidth(ctx)
             if ImGui_IsKeyPressed(ctx, ImGui_Key_Enter()):
                 with io.StringIO() as outbuf, io.StringIO() as errbuf, redirect_stdout(outbuf), redirect_stderr(errbuf):
                     interp.runsource(input_string)
                     output_string = outbuf.getvalue()
                     error_string = errbuf.getvalue()
+                console.exec(input_string)
                 history.append(("input", input_string))
                 if output_string: history.append(("output", output_string))
                 if error_string: history.append(("error", error_string))
@@ -110,6 +167,7 @@ def loop():
         reapy.defer(loop)
 
 def gui_test():
-    global ctx, input_string
+    global ctx, input_string, console
     input_string = ""
     ctx = ImGui_CreateContext('My script')
+    console = Console()
